8fc035e4b82bc1c3b9545526bd58c9e1c1beb4a1
marina <marina.ar1308@gmail.com>
2025-01-20 20:08:53 -0300

    currículo

---
+++ script.js
@@ -0,0 +1,12 @@
+const Button = document.querySelector('#button__Content')
+
+Button.addEventListener("click", () =>{
+
+    const link = document.createElement("a");
+    link.href = "arquivo/CV.pdf"
+    link.download = "currículo.pdf"
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link)
+
+});
